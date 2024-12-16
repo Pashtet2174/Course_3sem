d83a84adcf03eeff93fee2402237be4a1a3dfce4
Павел <pavel.obelenets@gmail.com>
2024-12-17 03:53:26 +0700

    add rent

--- Course_3sem/Rent.cs
+++ Course_3sem/Rent.cs
@@ -2,5 +2,10 @@ namespace Course_3sem;
 
 public class Rent
 {
-    
+    public Cinema Cinema { get; set; }
+    public Film Film { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal RentalAmount { get; set; }
+    public decimal Penalty { get; set; }
 }
